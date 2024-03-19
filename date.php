19d7877039f4ade47cc27487ef2f693c028d0c21
Mohit Sarkar <mohitsarkar256@gmail.com>
2024-03-19 22:57:48 +0530

    Hello world

--- date.php
+++ date.php
@@ -3,9 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Date Function</title>
 </head>
 <body>
-    
+    <h1>This is introduction about date function in php</h1>
+
+    <?php
+        $d= date("d-m-Y");
+        echo $d;
+    ?>
+
+    <h2>
+        <?php
+        $x= 34;
+        $today= date("d-m-Y");
+        echo "Today is ";
+        echo $today;
+        ?>
+        
+    </h2>
+
+
+    <?php
+        $arr1= array(
+            "Mohit" => "Neha",
+            "Pankaj"=> "Pinky",
+            1 => "Priya",
+            2=> 4
+        );
+
+        foreach ($arr1 as $key => $val){
+            echo "$key loves $val <br>";
+        }
+    ?>
+
 </body>
 </html>
